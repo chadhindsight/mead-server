48c6577596fa71a4bb985c4ccfa0d0d962166c8d
Chad Hinds <chadrickj8@gmail.com>
2020-02-06 17:23:48 -0500

    Add weather and about sections

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ app.get('', (req, res)=>{
     res.send('Hello Express')
 })
 
-app.get('/help', ()=>{
+app.get('/help', (req, res)=>{
     res.send('Help page')
 })
 
+app.get('/about', (req, res) => {
+    res.send('About page')
+})
+//Weather route
+app.get('/weather', (req, res) => {
+    res.send('Show weather')
+})
 
 app.listen(3000, ()=>{
     console.log('Server is up on port 3000!')
