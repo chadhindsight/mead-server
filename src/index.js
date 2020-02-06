b242c107a479b5d58bcc9f98bb08191d18129002
Chad Hinds <chadrickj8@gmail.com>
2020-02-06 10:44:58 -0500

    set up a port

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 // Allows you to use various methods stored on express
 const app = express();
 
-app.get('', ()=>{
-    
+app.get('', (req, res)=>{
+    res.send('Hello Express')
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000')
 })
