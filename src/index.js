a1ffe03f6abcfcc69dc8bb623ab0e5640508028c
Chad Hinds <chadrickj8@gmail.com>
2020-02-08 15:27:04 -0500

    lorganize code so it is more readable

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 const path = require('path');
 const express = require('express');
+
 // The variable app allows you to use various methods stored on express
 const app = express();
+// Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-
-app.use(express.static(publicDirectoryPath))
+const viewsPath = path.join(__dirname, '../templates')
+// Setup handlebars engine and views location
+app.set('views', viewsPath);
 app.set('view engine', 'hbs')
 
+// Setup static dir to serve
+app.use(express.static(publicDirectoryPath))
+
+
 app.get('', (req, res) =>{
-    res.render('index')
+    res.render('index', {
+        title: 'Weather App',
+        name: "Chad Hinds"
+    })
+})
+// About
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: "Chad Hinds"
+    })
+})
+// Help
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        msg: 'Have a query or need help with something? You are in the right place!'
+    })
 })
 //Weather route
 app.get('/weather', (req, res) => {
