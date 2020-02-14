cf13e8c4e43a7a255dd6d30d8c90f0dd7ff84466
Chad Hinds <chadrickj8@gmail.com>
2020-02-14 14:39:56 -0500

    set /*jshint esversion: 6 */

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+/*jshint esversion: 6 */
+
+const request = require('request');
+
+const forecast = (lat, long, callback)=>{
+    let url = `https://api.darksky.net/forecast/ec10e2a85b08f8ecda0c057b3c7227b1/${lat},${long}`;
+
+    request({ url, json: true }, (error, {body}) => {
+    if(error) {
+        callback('Unable to fetch weather data at this time')
+    }
+    else if(body.error) {
+        callback('unable to find location', undefined)
+    }
+    else {
+        callback(undefined, `${body.daily.data[0].summary} it is currently ${body.currently.temperature} There is currently ${body.currently.precipProbability}% chance of rain`)        
+    }
+});
+};
+
+module.exports = forecast;
