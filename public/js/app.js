9ffb89cf8160bc296668850362826f78f9d5edea
Chad Hinds <chadrickj8@gmail.com>
2020-02-19 17:25:03 -0500

    Remove unused console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,24 @@
 console.log('Client side javascript file is loaded')
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
+
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
 weatherForm.addEventListener('submit', (e) =>{
     e.preventDefault();
-    // Extract the input value
+    // Extract the input text from above value
     const location = search.value;
-    console.log('testing!')
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+
 })
