10a67d15bb7549f64efc266f1c8fa7246c52b8a5
Chad Hinds <chadrickj8@gmail.com>
2020-02-19 17:44:42 -0500

    Subsitute the text in messageOne for Two

--- public/js/app.js
+++ public/js/app.js
@@ -4,19 +4,22 @@ console.log('Client side javascript file is loaded')
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) =>{
     e.preventDefault();
     // Extract the input text from above value
     const location = search.value;
-
+    messageOne.textContent = 'Now loading...'
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
+
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })
